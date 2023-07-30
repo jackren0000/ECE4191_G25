224d2ac3b02537a9282ae432ea00ad47b372c573
jenakcr <91411340+jackren0000@users.noreply.github.com>
2023-07-30 20:00:13 +1000

    Update yolo_QRdetectionAndNavigation.py

--- yolo_QRdetectionAndNavigation.py
+++ yolo_QRdetectionAndNavigation.py
@@ -30,17 +30,23 @@ class Robot:
             qr_center_y = top + height / 2
             qr_area = width * height
 
-            if self.x < qr_center_x:
-                self.x += 1
-            elif self.x > qr_center_x:
-                self.x -= 1
+            # If QR code's center_x is in the left half of the image, move left
+            if qr_center_x < frame_width / 2:
+                # Replace with actual command to move robot left
+                print("Moving left")
 
-            if self.y < qr_center_y:
-                self.y += 1
-            elif self.y > qr_center_y:
-                self.y -= 1
+            # If QR code's center_x is in the right half of the image, move right
+            elif qr_center_x > frame_width / 2:
+                # Replace with actual command to move robot right
+                print("Moving right")
 
-            print(f"Moved to {self.x}, {self.y}")
+            # If QR code's area is smaller than the target area, move forward
+            if qr_area < self.target_area:
+                # Replace with actual command to move robot forward
+                print("Moving forward")
+            else:
+                print("Reached the QR code, stopping movement.")
+                break
 
 # Initialize the camera
 cap = cv2.VideoCapture(1)
