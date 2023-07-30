556c3a16292741f7af610f5dc85e9d051d7c0b2c
jenakcr <91411340+jackren0000@users.noreply.github.com>
2023-07-30 19:43:46 +1000

    Update yolo_QRdetectionAndNavigation.py

--- yolo_QRdetectionAndNavigation.py
+++ yolo_QRdetectionAndNavigation.py
@@ -2,14 +2,34 @@ import cv2
 from pyzbar.pyzbar import decode
 
 class Robot:
-    def __init__(self, x, y):
+    def __init__(self, x, y, cap):
         self.x = x
         self.y = y
-        # pre-defined a rectangle area
-        self.target_area = 100  
+        self.cap = cap
+        self.target_area = 100
 
-    def navigate_to(self, qr_center_x, qr_center_y, qr_area):
+    def navigate_to(self):
+        qr_area = 0
         while qr_area < self.target_area:
+            # Capture a frame from the camera
+            _, frame = self.cap.read()
+
+            # Decode QR codes from the frame
+            codes = decode(frame)
+
+            # If no codes are detected, raise an error
+            if not codes:
+                raise ValueError("No QR code detected")
+            
+            # Otherwise, take the detected QR code
+            code = codes[0]
+
+            # Calculate the center of the QR code and its area
+            left, top, width, height = code.rect
+            qr_center_x = left + width / 2
+            qr_center_y = top + height / 2
+            qr_area = width * height
+
             if self.x < qr_center_x:
                 self.x += 1
             elif self.x > qr_center_x:
@@ -30,38 +50,9 @@ if not cap.isOpened():
     raise IOError("Cannot open camera")
 
 # Initialize the robot at (0, 0)
-robot = Robot(0, 0)
+robot = Robot(0, 0, cap)
 
-while True:
-    # Capture a frame from the camera
-    _, frame = cap.read()
-
-    # Decode QR codes from the frame
-    codes = decode(frame)
-
-    # If no codes are detected, raise an error
-    if not codes:
-        raise ValueError("No QR code detected")
-    
-    # Otherwise, take the detected QR code
-    code = codes[0]
-
-    print(f"Detected QR code: {code.data}")
-
-    # Calculate the center of the QR code and its area
-    left, top, width, height = code.rect
-    qr_center_x = left + width / 2
-    qr_center_y = top + height / 2
-    qr_area = width * height
-
-    robot.navigate_to(qr_center_x, qr_center_y, qr_area)
-
-    # Display the image
-    cv2.imshow('frame', frame)
-
-    # Break the loop if 'q' is pressed
-    if cv2.waitKey(1) and 0xff == ord('q'):
-        break
+robot.navigate_to()
 
 # Release the camera for others to use
 cap.release()
