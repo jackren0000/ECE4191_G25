2f5d9ded8761bf7cd64841c2d04a35956822056e
jenakcr <91411340+jackren0000@users.noreply.github.com>
2023-08-02 20:43:29 +1000

    Update CheckTheQRinformation.py

--- test/CheckTheQRinformation.py
+++ test/CheckTheQRinformation.py
@@ -10,58 +10,47 @@ class Robot:
 
     def navigate_to(self):
         qr_area = 0
+        tolerance = 0.1  # 10% tolerance
         while qr_area < self.target_area:
-            # Capture a frame from the camera
             _, frame = self.cap.read()
-
-            # Decode QR codes from the frame
             codes = decode(frame)
 
-            # If no codes are detected, continue to the next frame
             if not codes:
                 print("No QR code detected")
                 continue
 
-            # Print information for all detected QR codes
             for code in codes:
-                # Calculate the center of the QR code and its area
                 left, top, width, height = code.rect
                 qr_center_x = int(left + width / 2)
                 qr_center_y = int(top + height / 2)
                 qr_area = width * height
 
-                # Print QR code information
                 print(f"QR Code Information:")
                 print(f"Data: {code.data.decode('utf-8')}")
                 print(f"Type: {code.type}")
                 print(f"Coordinates: {code.polygon}")
-                print(f"Area: {qr_area}")  # Prints the size of the QR code's area
+                print(f"Area: {qr_area}")  
 
-                # Draw rectangle around the QR code
                 cv2.rectangle(frame, (left, top), (left + width, top + height), (0, 255, 0), 3)
-
-                # Draw center point of the QR code
                 cv2.circle(frame, (qr_center_x, qr_center_y), radius=5, color=(0, 0, 255), thickness=-1)
 
-            # Display the resulting frame
             cv2.imshow('QR Code Detection', frame)
 
-            # If QR code's center_x is in the left half of the image, move left
-            if qr_center_x < frame.shape[1] / 2:
-                # Replace with actual command to move robot left
+            frame_center_x = frame.shape[1] / 2
+            left_limit = frame_center_x - frame.shape[1] * tolerance
+            right_limit = frame_center_x + frame.shape[1] * tolerance
+
+            # If QR code's center_x is in the left half of the image and outside the tolerance range, move left
+            if qr_center_x < left_limit:
                 print("Moving left")
 
-            # If QR code's center_x is in the right half of the image, move right
-            elif qr_center_x > frame.shape[1] / 2:
-                # Replace with actual command to move robot right
+            # If QR code's center_x is in the right half of the image and outside the tolerance range, move right
+            elif qr_center_x > right_limit:
                 print("Moving right")
 
-            # If QR code's area is smaller than the target area, move forward
             if qr_area < self.target_area:
-                # Replace with actual command to move robot forward
                 print("Moving forward")
 
-            # Break the loop if 'q' is pressed
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
