f112069c5e19089764fdd0c5155b698344531a01
jenakcr <91411340+jackren0000@users.noreply.github.com>
2023-08-04 16:20:21 +1000

    Update MotorControl_test.py

--- test/MotorControl_test.py
+++ test/MotorControl_test.py
@@ -29,21 +29,22 @@ def stop():
     pwm1.start(0)
     pwm2.start(0)
 
-# Test the motor
-print("Spinning motor forward")
-forward(50)  # Spin forward at half speed
-time.sleep(2)  # Continue for 2 seconds
-
-print("Stopping motor")
-stop()  # Stop
-time.sleep(1)  # Wait for 1 second
-
-print("Spinning motor backward")
-backward(50)  # Spin backward at half speed
-time.sleep(2)  # Continue for 2 seconds
-
-print("Stopping motor")
-stop()  # Stop
+for i in range(1000):
+    # Test the motor
+    print("Spinning motor forward")
+    forward(50)  # Spin forward at half speed
+    time.sleep(2)  # Continue for 2 seconds
+    
+    print("Stopping motor")
+    stop()  # Stop
+    time.sleep(1)  # Wait for 1 second
+    
+    print("Spinning motor backward")
+    backward(50)  # Spin backward at half speed
+    time.sleep(2)  # Continue for 2 seconds
+    
+    print("Stopping motor")
+    stop()  # Stop
 
 # Cleanup the GPIO pins
 GPIO.cleanup()
