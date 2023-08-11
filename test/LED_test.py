44fdeca6e77da131fa6964d37dc80f5b0551a33a
jenakcr <91411340+jackren0000@users.noreply.github.com>
2023-08-11 13:39:33 +1000

    Create LED_test.py

---
+++ test/LED_test.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+PWM1 = 17 # replace with your actual pin number
+
+# Set the GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+# Create PWM object
+p = GPIO.PWM(EN, 100)  # 100Hz frequency
+
+while (True):
+  p.start(50)  # Start with 50% duty cycle
+  time.sleep(2)  # run for 2 seconds
+  p.stop()  # Stop PWM
+  
