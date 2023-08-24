1bf72bb2f9afd95bc6b9786e63183a1ccee39477
jenakcr <91411340+jackren0000@users.noreply.github.com>
2023-08-24 20:34:20 +1000

    Update MotorControl_test2.py

--- test/MotorControl_test2.py
+++ test/MotorControl_test2.py
@@ -23,9 +23,21 @@ encoder = RotaryEncoder(20, 21, max_steps=1000000)
 # Create PWM object
 p = GPIO.PWM(EN, 100)  # 100Hz frequency
 
+# Wheel parameters (replace with your actual values)
+steps_per_rotation = 75  # number of encoder steps per full rotation
+wheel_diameter = 0.1  # wheel diameter in meters
+
+def calculate_speed(steps, time_interval):
+    # Calculate the distance traveled in the time interval
+    rotations = steps / steps_per_rotation
+    distance = rotations * wheel_diameter * 3.14159  # distance = circumference * number of rotations
+    speed = distance / time_interval  # speed = distance / time
+    return speed
+
 try:
     while True:
-        
+        initial_steps = encoder.steps
+
         # Forward/coast operation
         GPIO.output(ENB, GPIO.LOW)
         GPIO.output(PWM1, GPIO.HIGH)
@@ -34,15 +46,23 @@ try:
         time.sleep(2)  # run for 2 seconds
         p.stop()  # Stop PWM
 
-        print(f"Encoder steps, {encoder.steps}")
-        
+        final_steps = encoder.steps
+        speed = calculate_speed(final_steps - initial_steps, 2)
+        print(f"Forward Speed: {speed} m/s")
+
+        initial_steps = encoder.steps
+
         # Reverse/coast operation
         GPIO.output(ENB, GPIO.LOW)
         GPIO.output(PWM1, GPIO.LOW)
         GPIO.output(PWM2, GPIO.HIGH)
-        p.start(80)  # Start with 50% duty cycle
+        p.start(80)  # Start with 80% duty cycle
         time.sleep(2)  # run for 2 seconds
         p.stop()  # Stop PWM
+
+        final_steps = encoder.steps
+        speed = calculate_speed(initial_steps - final_steps, 2)
+        print(f"Reverse Speed: {speed} m/s")
 
         '''
         # Coast operation
@@ -51,6 +71,7 @@ try:
         GPIO.output(PWM2, GPIO.LOW)
         time.sleep(2)  # coast for 2 seconds
         '''
+
 except KeyboardInterrupt:
     # Stop the motor and clean up GPIO state
     p.stop()
