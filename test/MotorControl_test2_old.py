906c2b43ecb0fc42df344db99dfb99acea2707a4
jenakcr <91411340+jackren0000@users.noreply.github.com>
2023-08-25 13:45:21 +1000

    Create MotorControl_test2_old.py

---
+++ test/MotorControl_test2_old.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as GPIO
+import time
+# Set the GPIO mode
+GPIO.setmode(GPIO.BCM)
+# Define your motor control pins
+EN = 22   # replace with your actual pin number
+ENB = 23  # replace with your actual pin number
+PWM1 = 17 # replace with your actual pin number
+PWM2 = 27 # replace with your actual pin number
+# Set up the motor control pins zz
+GPIO.setup(EN, GPIO.OUT)
+GPIO.setup(ENB, GPIO.OUT)
+GPIO.setup(PWM1, GPIO.OUT)
+GPIO.setup(PWM2, GPIO.OUT)
+# Create PWM object
+p = GPIO.PWM(EN, 100)  # 100Hz frequency
+
+try:
+    while True:
+        '''
+
+        # Forward/coast operation
+        GPIO.output(ENB, GPIO.LOW)
+        GPIO.output(PWM1, GPIO.HIGH)
+        GPIO.output(PWM2, GPIO.LOW)
+        p.start(50)  # Start with 50% duty cycle
+        time.sleep(2)  # run for 2 seconds
+        p.stop()  # Stop PWM
+        '''
+
+
+        # Reverse/coast operation
+        GPIO.output(ENB, GPIO.LOW)
