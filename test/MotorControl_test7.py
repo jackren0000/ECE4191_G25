a85d209314b0f54bc70c2f8f0d5bd7b138638f2c
jenakcr <91411340+jackren0000@users.noreply.github.com>
2023-08-25 14:24:43 +1000

    Update MotorControl_test7.py

--- test/MotorControl_test7.py
+++ test/MotorControl_test7.py
@@ -25,7 +25,7 @@ def compute_speed(steps, time_interval):
 
 # Drive the robot forward for 2 seconds and compute speed
 robot.forward()
-time.sleep(2)
+time.sleep(10)
 robot.stop()
 
 # Compute speed of left wheel
